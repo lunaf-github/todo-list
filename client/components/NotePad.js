0f8131218d6e45ea3c788d45ba5f93e95e6a76fc
lunaf-github <lunaf91@gmail.com>
2022-03-11 19:08:18 -0800

    Aded form and bullet points

--- client/components/NotePad.js
+++ client/components/NotePad.js
@@ -1,20 +1,26 @@
-  // const root = document.getElementById("root");
-  // const h1tag = document.createElement("h1");
-  // root.appendChild(h1tag);
-  // h1tag.innerText = "Hello worlds"
+import Create from "./ElementCreators";
+import Task from "./Task";
+
+
 class NotePad {
- 
- 
- static appendToDiv(className){
-    const notePad = document.getElementsByClassName(className);
-    const task = document.createElement("h1");
-    task.innerText = "my first task"
-    for(let i=0; i<notePad.length; i++){
-     notePad[i].appendChild(task);
+
+ static load(className, tasks){
+    Create.element("form","form","class","note-pad","");
+    Create.element("input","textbox","class","note-pad","");
+    Create.element("button","button","class","note-pad","Add Task");
+
+    Create.div("tasks","class","note-pad");
+    
+    for(let i=0; i < tasks.length; i++){
+      Create.div("bullet", "class", "tasks")
+      Task.newTask("text","class","bullet",tasks[i])
     }
+
  }
 
-
 }
+
+
+
 
 export default NotePad;
