60a832e585f4e50cca1e2c6e4d54f0838d2551be
Fernando <lunaf91@gmail.com>
2023-10-10 12:05:32 -0700

    Clean up code

--- model/taskModel.js
+++ model/taskModel.js
@@ -1,14 +1,6 @@
 const mongoose = require('mongoose');
 
-
 const taskSchema = new mongoose.Schema({ task: String })
-
-
-// const Schema = mongoose.Schema;
-
-// const taskSchema = new Schema({
-//   tasks:{type:Array, required:true}
-// });
 
 const Task = mongoose.model('task', taskSchema);
 
