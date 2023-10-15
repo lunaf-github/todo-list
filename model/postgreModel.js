a7f4b803f56edd164f2c10b4c1ca36f8b23de780
Fernando <lunaf91@gmail.com>
2023-10-14 23:38:24 -0700

    Add event listeners for connect and error

--- model/postgreModel.js
+++ model/postgreModel.js
@@ -8,6 +8,26 @@ const pool = new Pool({
     connectionString: process.env.PG_URI
 });
 
+// I'm not sure if this is standard practice, but I needed this IIFE to test my PG DB connectoin upon
+// starting my server. 
+(async function testPGConnection() {
+    // Creates a client which starts the DB connection (an object that allows me to query to database)
+    const client  = await pool.connect();
+    // Destroy the client, I don't want the client to use up a spot in my pool
+    client.release(true);
+})();
+
+// The pool is an event emmiter, so we can create an event listener for the 'connect' event. 
+// This is used to let the developer know that our pg database has succesfully connected. 
+pool.on('connect', () => console.log('Connected to Postgres'));
+
+// This will catch any errors with our database connection. It is important to include this because
+// if  pool emits an error event and no listeners are added , node will emit an uncaught error and
+// potentially crash your node process
+pool.on('error', (error, client) => {
+    console.log(`Server down:  ${error}`);
+})
+
 module.exports = {
     query: (text, params, callback) => {
         return pool.query(text, params, callback);
